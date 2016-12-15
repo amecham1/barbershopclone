037aee28ec5969b0b2f373f8b1095b12e035a09e
Andrew <andrew.mecham@gmail.com>
2016-12-15 16:17:53 -0700

    added webpack and babel

---
+++ js/BarberApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import { BrowserRouter, Match } from 'react-router'
+
+const App = React.createClass({
+  render () {
+    return (
+  
+    )
+  }
+})
+
+render(<App />, document.getElementById('app'))
