a18fb4ea60033c3fcb382246f9796a2ecf78289a
Andrew <andrew.mecham@gmail.com>
2017-01-05 13:59:23 -0700

    working on ham menu

---
+++ js/components/menu/HamMenu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './menu.scss'
+
+class HamMenu extends React.Component {
+  render () {
+    return (
+      <div className='ham-menu'>
+        <img src='../js/img/Ham Menu.png' />
+      </div>
+    )
+  }
+}
+
+export default HamMenu
--- js/components/menu/MenuBar.js
+++ js/components/menu/MenuBar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './menu.scss'
 import Title from '../menu/Title'
+import HamMenu from '../menu/HamMenu'
 
 class MenuBar extends React.Component {
   render () {
     return (
       <div className='header'>
+        <HamMenu />
         <Title />
+
       </div>
     )
   }
